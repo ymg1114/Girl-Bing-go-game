e18360d6852c8d987dbc1bf81a64f1f790dcd08d
DESKTOP-MHUM7TN\skdid <skdid8419@gmail.com>
2021-07-24 18:10:23 +0900

    아직 빌드 명령어가 작동하지 않음.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 set root=C:\Users\skdid\anaconda3
 
-call %root%\Scripts\activate.bat %root%
+call %root%\Scripts\activate.bat
 
 set pg1=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6\plugins
 set pg2=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6
@@ -9,14 +9,16 @@ set shi=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\shiboken6
 
 call conda activate ymg
 
-pyinstaller --name="Girl bing-go" ^
+pip install -r requirements.txt
 
+pyinstaller --name="Girl bing-go" 
 --add-data "%pg1%; PySide6\plugins" ^
 --add-data "%pg2%\*.dll; PySide6" ^
+--add-data "%pg2%\*.pyd; PySide6" ^
 --add-data "%shi%\*.dll; shiboken6" ^
-
 --add-data ".\girl; girl" ^
 --add-data ".\nice; nice" ^
 --onefile ^
+--windowed ^
 ymg_pyside_mini_project1.py 
 pause
