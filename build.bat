ad392239ed54f162175a6c6e9f5d6d51c10459ce
DESKTOP-MHUM7TN\skdid <skdid8419@gmail.com>
2021-07-24 19:12:14 +0900

    build 수정 (아직 안됨). 윈터 아이콘 추가.

--- build.bat
+++ build.bat
@@ -1,17 +1,16 @@
 set root=C:\Users\skdid\anaconda3
-
 call %root%\Scripts\activate.bat
 
 set pg1=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6\plugins
 set pg2=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6
-
 set shi=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\shiboken6
 
 call conda activate ymg
 
 pip install -r requirements.txt
 
-pyinstaller --name="Girl bing-go" ^
+pyinstaller ^
+--name="Girl bing-go" ^
 --add-data="%pg1%; PySide6" ^
 --add-data="%pg2%\*.dll; PySide6" ^
 --add-data="%pg2%\*.pyd; PySide6" ^
@@ -22,5 +21,6 @@ pyinstaller --name="Girl bing-go" ^
 --add-data=".\nice\*.jpg; nice" ^
 --onefile ^
 --windowed ^
+-i=".\winter.ico" ^
 ymg_pyside_mini_project1.py 
 pause
