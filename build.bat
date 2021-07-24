aade29d2fbff8fa6453f2cf9f74a4207647b6257
DESKTOP-MHUM7TN\skdid <skdid8419@gmail.com>
2021-07-24 17:53:21 +0900

    빌드 명령어 수정 (아직 동작하지 않음)

--- build.bat
+++ build.bat
@@ -2,15 +2,21 @@ set root=C:\Users\skdid\anaconda3
 
 call %root%\Scripts\activate.bat %root%
 
+set pg1=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6\plugins
+set pg2=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\PySide6
+
+set shi=C:\Users\skdid\Anaconda3\envs\ymg\Lib\site-packages\shiboken6
+
 call conda activate ymg
 
 pyinstaller --name="Girl bing-go" ^
--p="C:\Users\skdid\anaconda3\envs\ymg\Lib\site-packages\PySide6\plugins/platforms" ^
---add-data="C:\Users\skdid\anaconda3\envs\ymg\Lib\site-packages\PySide6\plugins/platforms" ^
---add-data="C:\Users\skdid\Documents\Girl-Bing-go-game\girl; C:\Users\skdid\Documents\Girl-Bing-go-game\girl" ^
---add-data="C:\Users\skdid\Documents\Girl-Bing-go-game\nice; C:\Users\skdid\Documents\Girl-Bing-go-game\nice" ^
+
+--add-data "%pg1%; PySide6\plugins" ^
+--add-data "%pg2%\*.dll; PySide6" ^
+--add-data "%shi%\*.dll; shiboken6" ^
+
+--add-data ".\girl; girl" ^
+--add-data ".\nice; nice" ^
 --onefile ^
 ymg_pyside_mini_project1.py 
 pause
-
-C:\Users\skdid\Documents\Girl-Bing-go-game
