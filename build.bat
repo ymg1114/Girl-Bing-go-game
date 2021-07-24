004ea5dba5e7d9af46f1a0f7bb69d8191b9b9f8a
DESKTOP-MHUM7TN\skdid <skdid8419@gmail.com>
2021-07-24 18:23:48 +0900

    build 버그 수정 (아직 안됨)

--- build.bat
+++ build.bat
@@ -11,13 +11,15 @@ call conda activate ymg
 
 pip install -r requirements.txt
 
-pyinstaller --name="Girl bing-go" 
---add-data "%pg1%; PySide6\plugins" ^
---add-data "%pg2%\*.dll; PySide6" ^
---add-data "%pg2%\*.pyd; PySide6" ^
---add-data "%shi%\*.dll; shiboken6" ^
---add-data ".\girl; girl" ^
---add-data ".\nice; nice" ^
+pyinstaller --name="Girl bing-go" ^
+--add-data="%pg1%; PySide6" ^
+--add-data="%pg2%\*.dll; PySide6" ^
+--add-data="%pg2%\*.pyd; PySide6" ^
+--add-data="%pg2%\pyside6.*.dll; PySide6" ^
+--add-data="%shi%\*.dll; shiboken6" ^
+--add-data="%shi%\shiboken6.*.dll; shiboken6" ^
+--add-data=".\girl\*.jpg; girl" ^
+--add-data=".\nice\*.jpg; nice" ^
 --onefile ^
 --windowed ^
 ymg_pyside_mini_project1.py 
